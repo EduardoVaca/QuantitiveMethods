186c09976cb9ea4eaaeca03ab0490544cf860266
EduardoVaca <eduardo.vaca.g@gmail.com>
2018-01-29 11:40:27 -0600

    Add mean func

--- formulario.py
+++ formulario.py
@@ -12,5 +12,13 @@ def get_permutations_count(n=None, r=None):
     return math.floor(math.factorial(n)/math.factorial(n-r))
 
 
-print(get_combinations_count())
-print(get_permutations_count())
+def get_mean(prob_list=None):
+    if not prob_list:
+        n = int(input('n of values:'))
+        prob_list = [(int(input('x{}: '.format(i+1))), float(input('p(x{}): '.format(i+1)))) for i in range(n)]
+    return sum(x*y for x,y in prob_list)
+
+
+#print(get_combinations_count())
+#print(get_permutations_count())
+print(get_mean())
