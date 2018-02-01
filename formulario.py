de67d9e3baeb29f140a8e065572884dda3e79667
EduardoVaca <eduardo.vaca.g@gmail.com>
2018-02-01 10:31:24 -0600

    Add comments

--- formulario.py
+++ formulario.py
@@ -2,28 +2,66 @@ import math
 
 
 def get_combinations_count(n=None, r=None):
+    """Computes the total possible combinations
+    PARAMS:
+    - n : N
+    - r : R
+    RETURNS:
+    Number of combinations
+    """
     if  n is None or r is None:
         n, r = int(input('n:')), int(input('r:'))
     return math.floor(math.factorial(n)/(math.factorial(r)*math.factorial(n-r)))
 
 def get_permutations_count(n=None, r=None):
+    """Computes the total possible permutations
+    PARAMS:
+    - n : N
+    - r : R
+    RETURNS:
+    Number of permutations
+    """
     if n is None or r is None:
         n, r = int(input('n:')), int(input('r:'))
     return math.floor(math.factorial(n)/math.factorial(n-r))
 
 def get_prob_list():
+    """Input the prob list
+    RETURNS:
+    - list of tuples (x, prob(x))
+    """
     return [(int(input('x{}: '.format(i+1))), float(input('p(x{}): '.format(i+1)))) for i in range(int(input('n of values:')))]
 
 def get_mean(prob_list=None):
+    """Compute the mean of a prob list
+    PARAMS:
+    - prob_list : list of tuples with (x, prob(x))
+    RETURNS:
+    - mean of list
+    """
     prob_list = get_prob_list() if not prob_list else prob_list   
     return sum(x*y for x,y in prob_list)
 
 def get_variance(prob_list=None, mean=None):
+    """Compute the variance of a prob list
+    PARAMS:
+    - prob_list : list of tuples with (x, prob(x))
+    - mean : mean of list
+    RETURNS:
+    - variance of list
+    """
     prob_list = get_prob_list() if not prob_list else prob_list
     mean = get_mean(prob_list) if not mean else mean
     return sum(math.pow(x-mean, 2)*y for x,y in prob_list)
 
-def get_standard_dev(variance=None, prob_list=None):    
+def get_standard_dev(variance=None, prob_list=None):
+    """Compute the standard devaition of a prob list
+    PARAMS:
+    - variance : variance of list
+    - prob_list : list of tuples with (x, prob(x))
+    RETURNS:
+    - standard dev of list
+    """  
     return math.sqrt(get_variance(prob_list, mean=None) if not variance else variance)
 
 def binomial_distribution(n, k, p, q):
@@ -33,6 +71,8 @@ def binomial_distribution(n, k, p, q):
     - k : list of success
     - p : success prob
     - q : failure prob
+    RETURNS:
+    - Binomial dist
     """
     return sum(get_combinations_count(n, x)*math.pow(p, x)*math.pow(q, n-x) for x in k)
 
