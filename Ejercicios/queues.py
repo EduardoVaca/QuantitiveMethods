beec8aa38a84703923505e24af40f4c3314a2489
EduardoVaca <eduardo.vaca.g@gmail.com>
2018-03-14 16:54:39 -0600

    Add functions for model M/M/S

--- Ejercicios/queues.py
+++ Ejercicios/queues.py
@@ -37,7 +37,7 @@ def utility_factor(lam, mu, s=None):
     RETURNS:
     - utility factor
     """
-    return lam/mu if s is None else lam/s*mu
+    return lam/mu if s is None else lam/(s*mu)
 
 def mm1_performance(lam, mu):
     """Computes performance mesures from M/M/1 model
@@ -51,7 +51,7 @@ def mm1_performance(lam, mu):
     results['Ls'] = lam/(mu-lam)
     results['Lq'] = pow(lam,2)/(mu*(mu-lam))
     results['Ws'] = 1/(mu-lam)
-    results['Wq'] = lam/(mu*(mu-lam)) 
+    results['Wq'] = lam/(mu*(mu-lam))
     return results
 
 def mm1_prob_queue_len(lam, mu, n):
@@ -80,6 +80,47 @@ def mm1_prob_queue_wait(lam, mu, t):
     results.append(('P(Ws > {})'.format(t), math.exp(-1*mu*(1-p)*t)))
     results.append(('P(Wq > {})'.format(t), p*math.exp(-1*mu*(1-p)*t)))
     return results
+
+def mms_performance(lam, mu, s):
+    """Computes performance mesures from M/M/S model
+    PARAMS:
+    - lam : rate of arrivals
+    - mu : rate of service
+    - s : number of servers to operate
+    RETURNS:
+    - Performance measures
+    """
+    results = {'lam': lam, 'mu': mu, 's': s}
+    results['Lq'] = (pow(lam/mu, s)*mms_p0(lam, mu, s)*utility_factor(lam, mu, s=s))/(math.factorial(s)*pow(1-utility_factor(lam, mu, s=s), 2))
+    results['Wq'] = results['Lq']/lam
+    results['Ws'] = results['Wq'] + 1/mu
+    results['Ls'] = lam*results['Ws']
+    return results
+
+
+def mms_p0(lam, mu, s):
+    """Computes prob of 0 servers
+    PARAMS:
+    - lam : rate of arrivals
+    - mu : rate of service
+    - s : number of servers to operate
+    RETURNS:
+    - Prob of 0 servers
+    """
+    return 1 / (sum(pow(lam/mu, n)/math.factorial(n) for n in range(s)) + pow(lam/mu, s)/(math.factorial(s)*(1-utility_factor(lam, mu, s=s))))
+
+def mms_pn(lam, mu, s, n_s):
+    """Computes prob of n servers
+    PARAMS:
+    - lam : rate of arrivals
+    - mu : rate of service
+    - s : number of servers to operate
+    - n_s : list of servers to obtain prob
+    RETURNS:
+    - Prob of n servers
+    """
+    p0 = mms_p0(lam, mu, s)
+    return [(pow(lam/mu, n)*p0)/math.factorial(n) for n in n_s if n >= 0 and n <= s] + [(pow(lam/mu, n)*p0)/(math.factorial(s)*pow(s, n-s)) for n in n_s if n > s]
 
 
 
